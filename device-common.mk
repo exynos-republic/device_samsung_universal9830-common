75439f5bb85cb3a3effdec616272130cf1861eee
Tim Zimmermann <tim@linux4.de>
2023-07-23 20:42:33 +0200

    universal9830: Build protobuf 3.9.1-vendorcompat
    * Required by RIL and widevine DRM
    * Patch widevine to load full variant as there's no lite vendorcompat variant
    
    Change-Id: I8d1fbc40bebf66abea342780b02cda68d64899b1
    Signed-off-by: Francescodario Cuzzocrea <bosconovic@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -287,6 +287,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.recovery.exynos990.rc
 
+# Protobuf
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full-3.9.1-vendorcompat
+
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.4.vendor:64 \
