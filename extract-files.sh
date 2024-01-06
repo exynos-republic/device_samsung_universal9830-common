018ee1568d33b6dc3200f4dec2bf2d43a8a26286
Tim Zimmermann <tim@linux4.de>
2024-01-06 13:56:30 +0100

    universal9830: Commonize libsec-ril
    Change-Id: I2e857eeaa1739c76236a4d820585641b58d0672c
    Signed-off-by: Francescodario Cuzzocrea <bosconovic@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -71,6 +71,11 @@ function blob_fixup() {
         vendor/bin/hw/rild)
             "${PATCHELF}" --replace-needed libril.so libril-samsung.so "${2}"
             ;;
+        vendor/lib*/libsec-ril*.so)
+            xxd -p -c0 "${2}" | sed "s/600e40f9820c805224008052e10315aae30314aa/600e40f9820c805224008052e10315aa030080d2/g" | xxd -r -p > "${2}".patched
+            mv "${2}".patched "${2}"
+            "${PATCHELF}" --replace-needed libril.so libril-samsung.so "${2}"
+            ;;
         vendor/lib*/libsensorlistener.so)
             "${PATCHELF}" --add-needed libsensorndkbridge_shim.so "${2}"
             ;;
