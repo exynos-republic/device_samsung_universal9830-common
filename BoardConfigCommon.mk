46fa4019d5514228f648d91f4848f257d75dddb1
Tim Zimmermann <tim@linux4.de>
2024-03-21 19:51:22 +0100

    universal9830: Remove DEXPREOPT_GENERATE_APEX_IMAGE
    * Removed in https://android-review.googlesource.com/c/platform/build/+/1230120
    
    Change-Id: I0db83d78ac34969a130975e0f730cfec9a1f55d4
    Signed-off-by: Francescodario Cuzzocrea <bosconovic@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -16,9 +16,6 @@
 
 COMMON_PATH := device/samsung/universal9830-common
 
-# APEX
-DEXPREOPT_GENERATE_APEX_IMAGE := true
-
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a-dotprod
