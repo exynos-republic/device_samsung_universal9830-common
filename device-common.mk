fcuzzocrea/android_device_samsung_universal9830-common

universal9830: build rebalance interrupts daemon

device-common.mk
@@ -281,6 +281,10 @@ PRODUCT_PACKAGES += \
     init.exynos990.rc \
     ueventd.exynos990.rc
 
+# Rebalance Interrupts
+PRODUCT_PACKAGES += \
+    rebalance_interrupts-vendor
+
 # Recovery
 PRODUCT_PACKAGES += \
     init.recovery.exynos990.rc

exynos-republic/device_samsung_universal9830-common

Change-Id: Ieb9819886daf7c3406350cfca2592a7658e63da6

device-common.mk
@@ -282,6 +282,10 @@ PRODUCT_PACKAGES += \
     init.exynos990.rc \
     ueventd.exynos990.rc
 
+# Rebalance Interrupts
+PRODUCT_PACKAGES += \
+    rebalance_interrupts-samsung
+
 # Recovery
 PRODUCT_PACKAGES += \
     init.recovery.exynos990.rc