e81f300b3e901cf809124f48b9821df374b1409c
Francescodario Cuzzocrea <bosconovic@gmail.com>
2024-04-27 17:47:35 +0200

    universal9830: update blobs from SJHXC1
    Change-Id: Ide077e7dfc16093186771a32c0200f3761a5eecf
    Signed-off-by: Francescodario Cuzzocrea <bosconovic@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -178,7 +178,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 BOARD_SECCOMP_POLICY += $(COMMON_PATH)/seccomp
 
 # Security
-VENDOR_SECURITY_PATCH := 2023-12-01
+VENDOR_SECURITY_PATCH := 2024-03-01
 
 # SELinux
 include device/lineage/sepolicy/exynos/sepolicy.mk
