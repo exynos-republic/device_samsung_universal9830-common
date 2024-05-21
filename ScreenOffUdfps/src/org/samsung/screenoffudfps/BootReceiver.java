Rocky7842/android_device_samsung_universal9830-common

[DNM] universal9830: Add ScreenOffUdfps app
* With the kernel change that force enable UDFPS when screen is on, we can now use this app to utilize the `fod_enable,1`/`fod_enable,0` command to control the on/off of screen off UDFPS.

ScreenOffUdfps/src/org/lineageos/screenoffudfps/BootReceiver.java
@@ -1,0 +1,42 @@
+/*
+ * Copyright (c) 2021-2022 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.lineageos.screenoffudfps;
+
+import org.lineageos.screenoffudfps.utils.FileUtils;
+import org.lineageos.screenoffudfps.utils.Tags;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.content.SharedPreferences;
+import androidx.preference.PreferenceManager;
+
+public class BootReceiver extends BroadcastReceiver {
+    private static final boolean DEBUG = false;
+    private static final String TAG = "ScreenOffUdfps";
+
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        if (DEBUG)
+            Log.d(TAG, "Received boot completed intent");
+
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+
+        boolean kScreenOffUdfps = sharedPrefs.getBoolean(Tags.SCREEN_OFF_UDFPS_KEY, false);
+        FileUtils.writeLine(Tags.SCREEN_OFF_UDFPS_NODE, kScreenOffUdfps?"fod_enable,1":"fod_enable,0");
+    }
+}

ScreenOffUdfps/src/org/lineageos/screenoffudfps/SettingsFragment.java
@@ -1,0 +1,68 @@
+package org.lineageos.screenoffudfps;
+
+import org.lineageos.screenoffudfps.R;
+import org.lineageos.screenoffudfps.utils.FileUtils;
+import org.lineageos.screenoffudfps.utils.Tags;
+
+import android.app.ActionBar;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.PreferenceManager;
+import androidx.preference.SeekBarPreference;
+import androidx.preference.SwitchPreference;
+
+public class SettingsFragment extends PreferenceFragment {
+
+    private SharedPreferences mSharedPrefs;
+    private SwitchPreference mScreenOffUdfpsPreference;
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.screenoffudfps_settings);
+
+        mSharedPrefs = PreferenceManager.getDefaultSharedPreferences(getContext());
+
+        mScreenOffUdfpsPreference = (SwitchPreference) findPreference(Tags.SCREEN_OFF_UDFPS_KEY);
+
+        if (FileUtils.fileExists(Tags.SCREEN_OFF_UDFPS_NODE)) {
+            mScreenOffUdfpsPreference.setEnabled(true);
+            mScreenOffUdfpsPreference.setSummary(R.string.screen_off_udfps_summary);
+        } else {
+            mScreenOffUdfpsPreference.setEnabled(false);
+            mScreenOffUdfpsPreference.setSummary(R.string.screen_off_udfps_not_supported);
+        }
+
+        mScreenOffUdfpsPreference.setOnPreferenceChangeListener((preference, newValue) -> {
+            FileUtils.writeLine(Tags.SCREEN_OFF_UDFPS_NODE, (boolean)newValue?"fod_enable,1":"fod_enable,0");
+            return true;
+        });
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        final View view = LayoutInflater.from(getContext()).inflate(R.layout.screenoffudfps,
+                container, false);
+        ((ViewGroup) view).addView(super.onCreateView(inflater, container, savedInstanceState));
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    private boolean getScreenOffUdfps() {
+        return mSharedPrefs.getBoolean(Tags.SCREEN_OFF_UDFPS_KEY, false);
+    }
+}

ScreenOffUdfps/src/org/lineageos/screenoffudfps/utils/FileUtils.java
@@ -1,0 +1,149 @@
+package org.lineageos.screenoffudfps.utils;
+
+import android.util.Log;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public final class FileUtils {
+  private static final String TAG = "FileUtils";
+
+  private FileUtils() {
+    // This class is not supposed to be instantiated
+  }
+
+  /**
+   * Reads the first line of text from the given file.
+   * Reference {@link BufferedReader#readLine()} for clarification on what a
+   * line is
+   *
+   * @return the read line contents, or null on failure
+   */
+  public static String readOneLine(String fileName) {
+    String line = null;
+    BufferedReader reader = null;
+
+    try {
+      reader = new BufferedReader(new FileReader(fileName), 512);
+      line = reader.readLine();
+    } catch (FileNotFoundException e) {
+      Log.w(TAG, "No such file " + fileName + " for reading", e);
+    } catch (IOException e) {
+      Log.e(TAG, "Could not read from file " + fileName, e);
+    } finally {
+      try {
+        if (reader != null) {
+          reader.close();
+        }
+      } catch (IOException e) {
+        // Ignored, not much we can do anyway
+      }
+    }
+
+    return line;
+  }
+
+  /**
+   * Writes the given value into the given file
+   *
+   * @return true on success, false on failure
+   */
+  public static boolean writeLine(String fileName, String value) {
+    BufferedWriter writer = null;
+
+    try {
+      writer = new BufferedWriter(new FileWriter(fileName));
+      writer.write(value);
+    } catch (FileNotFoundException e) {
+      Log.w(TAG, "No such file " + fileName + " for writing", e);
+      return false;
+    } catch (IOException e) {
+      Log.e(TAG, "Could not write to file " + fileName, e);
+      return false;
+    } finally {
+      try {
+        if (writer != null) {
+          writer.close();
+        }
+      } catch (IOException e) {
+        // Ignored, not much we can do anyway
+      }
+    }
+
+    return true;
+  }
+
+  /**
+   * Checks whether the given file exists
+   *
+   * @return true if exists, false if not
+   */
+  public static boolean fileExists(String fileName) {
+    final File file = new File(fileName);
+    return file.exists();
+  }
+
+  /**
+   * Checks whether the given file is readable
+   *
+   * @return true if readable, false if not
+   */
+  public static boolean isFileReadable(String fileName) {
+    final File file = new File(fileName);
+    return file.exists() && file.canRead();
+  }
+
+  /**
+   * Checks whether the given file is writable
+   *
+   * @return true if writable, false if not
+   */
+  public static boolean isFileWritable(String fileName) {
+    final File file = new File(fileName);
+    return file.exists() && file.canWrite();
+  }
+
+  /**
+   * Deletes an existing file
+   *
+   * @return true if the delete was successful, false if not
+   */
+  public static boolean delete(String fileName) {
+    final File file = new File(fileName);
+    boolean ok = false;
+    try {
+      ok = file.delete();
+    } catch (SecurityException e) {
+      Log.w(TAG, "SecurityException trying to delete " + fileName, e);
+    }
+    return ok;
+  }
+
+  /**
+   * Renames an existing file
+   *
+   * @return true if the rename was successful, false if not
+   */
+  public static boolean rename(String srcPath, String dstPath) {
+    final File srcFile = new File(srcPath);
+    final File dstFile = new File(dstPath);
+    boolean ok = false;
+    try {
+      ok = srcFile.renameTo(dstFile);
+    } catch (SecurityException e) {
+      Log.w(TAG,
+            "SecurityException trying to rename " + srcPath + " to " + dstPath,
+            e);
+    } catch (NullPointerException e) {
+      Log.e(TAG,
+            "NullPointerException trying to rename " + srcPath + " to " +
+                dstPath,
+            e);
+    }
+    return ok;
+  }
+}

ScreenOffUdfps/src/org/lineageos/screenoffudfps/utils/Tags.java
@@ -1,0 +1,6 @@
+package org.lineageos.screenoffudfps.utils;
+
+public class Tags {
+    public static final String SCREEN_OFF_UDFPS_KEY = "screen_off_udfps";
+    public static final String SCREEN_OFF_UDFPS_NODE = "/sys/class/sec/tsp/cmd";
+}

exynos-republic/device_samsung_universal9830-common

ScreenOffUdfps/src/org/samsung/screenoffudfps/BootReceiver.java
@@ -1,0 +1,42 @@
+/*
+ * Copyright (c) 2021-2022 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.samsung.screenoffudfps;
+
+import org.samsung.screenoffudfps.utils.FileUtils;
+import org.samsung.screenoffudfps.utils.Tags;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.content.SharedPreferences;
+import androidx.preference.PreferenceManager;
+
+public class BootReceiver extends BroadcastReceiver {
+    private static final boolean DEBUG = false;
+    private static final String TAG = "ScreenOffUdfps";
+
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        if (DEBUG)
+            Log.d(TAG, "Received boot completed intent");
+
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+
+        boolean kScreenOffUdfps = sharedPrefs.getBoolean(Tags.SCREEN_OFF_UDFPS_KEY, false);
+        FileUtils.writeLine(Tags.SCREEN_OFF_UDFPS_NODE, kScreenOffUdfps?"fod_enable,1":"fod_enable,0");
+    }
+}

ScreenOffUdfps/src/org/samsung/screenoffudfps/SettingsFragment.java
@@ -1,0 +1,68 @@
+package org.samsung.screenoffudfps;
+
+import org.samsung.screenoffudfps.R;
+import org.samsung.screenoffudfps.utils.FileUtils;
+import org.samsung.screenoffudfps.utils.Tags;
+
+import android.app.ActionBar;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.PreferenceManager;
+import androidx.preference.SeekBarPreference;
+import androidx.preference.SwitchPreference;
+
+public class SettingsFragment extends PreferenceFragment {
+
+    private SharedPreferences mSharedPrefs;
+    private SwitchPreference mScreenOffUdfpsPreference;
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.screenoffudfps_settings);
+
+        mSharedPrefs = PreferenceManager.getDefaultSharedPreferences(getContext());
+
+        mScreenOffUdfpsPreference = (SwitchPreference) findPreference(Tags.SCREEN_OFF_UDFPS_KEY);
+
+        if (FileUtils.fileExists(Tags.SCREEN_OFF_UDFPS_NODE)) {
+            mScreenOffUdfpsPreference.setEnabled(true);
+            mScreenOffUdfpsPreference.setSummary(R.string.screen_off_udfps_summary);
+        } else {
+            mScreenOffUdfpsPreference.setEnabled(false);
+            mScreenOffUdfpsPreference.setSummary(R.string.screen_off_udfps_not_supported);
+        }
+
+        mScreenOffUdfpsPreference.setOnPreferenceChangeListener((preference, newValue) -> {
+            FileUtils.writeLine(Tags.SCREEN_OFF_UDFPS_NODE, (boolean)newValue?"fod_enable,1":"fod_enable,0");
+            return true;
+        });
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        final View view = LayoutInflater.from(getContext()).inflate(R.layout.screenoffudfps,
+                container, false);
+        ((ViewGroup) view).addView(super.onCreateView(inflater, container, savedInstanceState));
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    private boolean getScreenOffUdfps() {
+        return mSharedPrefs.getBoolean(Tags.SCREEN_OFF_UDFPS_KEY, false);
+    }
+}

ScreenOffUdfps/src/org/samsung/screenoffudfps/utils/FileUtils.java
@@ -1,0 +1,149 @@
+package org.samsung.screenoffudfps.utils;
+
+import android.util.Log;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public final class FileUtils {
+  private static final String TAG = "FileUtils";
+
+  private FileUtils() {
+    // This class is not supposed to be instantiated
+  }
+
+  /**
+   * Reads the first line of text from the given file.
+   * Reference {@link BufferedReader#readLine()} for clarification on what a
+   * line is
+   *
+   * @return the read line contents, or null on failure
+   */
+  public static String readOneLine(String fileName) {
+    String line = null;
+    BufferedReader reader = null;
+
+    try {
+      reader = new BufferedReader(new FileReader(fileName), 512);
+      line = reader.readLine();
+    } catch (FileNotFoundException e) {
+      Log.w(TAG, "No such file " + fileName + " for reading", e);
+    } catch (IOException e) {
+      Log.e(TAG, "Could not read from file " + fileName, e);
+    } finally {
+      try {
+        if (reader != null) {
+          reader.close();
+        }
+      } catch (IOException e) {
+        // Ignored, not much we can do anyway
+      }
+    }
+
+    return line;
+  }
+
+  /**
+   * Writes the given value into the given file
+   *
+   * @return true on success, false on failure
+   */
+  public static boolean writeLine(String fileName, String value) {
+    BufferedWriter writer = null;
+
+    try {
+      writer = new BufferedWriter(new FileWriter(fileName));
+      writer.write(value);
+    } catch (FileNotFoundException e) {
+      Log.w(TAG, "No such file " + fileName + " for writing", e);
+      return false;
+    } catch (IOException e) {
+      Log.e(TAG, "Could not write to file " + fileName, e);
+      return false;
+    } finally {
+      try {
+        if (writer != null) {
+          writer.close();
+        }
+      } catch (IOException e) {
+        // Ignored, not much we can do anyway
+      }
+    }
+
+    return true;
+  }
+
+  /**
+   * Checks whether the given file exists
+   *
+   * @return true if exists, false if not
+   */
+  public static boolean fileExists(String fileName) {
+    final File file = new File(fileName);
+    return file.exists();
+  }
+
+  /**
+   * Checks whether the given file is readable
+   *
+   * @return true if readable, false if not
+   */
+  public static boolean isFileReadable(String fileName) {
+    final File file = new File(fileName);
+    return file.exists() && file.canRead();
+  }
+
+  /**
+   * Checks whether the given file is writable
+   *
+   * @return true if writable, false if not
+   */
+  public static boolean isFileWritable(String fileName) {
+    final File file = new File(fileName);
+    return file.exists() && file.canWrite();
+  }
+
+  /**
+   * Deletes an existing file
+   *
+   * @return true if the delete was successful, false if not
+   */
+  public static boolean delete(String fileName) {
+    final File file = new File(fileName);
+    boolean ok = false;
+    try {
+      ok = file.delete();
+    } catch (SecurityException e) {
+      Log.w(TAG, "SecurityException trying to delete " + fileName, e);
+    }
+    return ok;
+  }
+
+  /**
+   * Renames an existing file
+   *
+   * @return true if the rename was successful, false if not
+   */
+  public static boolean rename(String srcPath, String dstPath) {
+    final File srcFile = new File(srcPath);
+    final File dstFile = new File(dstPath);
+    boolean ok = false;
+    try {
+      ok = srcFile.renameTo(dstFile);
+    } catch (SecurityException e) {
+      Log.w(TAG,
+            "SecurityException trying to rename " + srcPath + " to " + dstPath,
+            e);
+    } catch (NullPointerException e) {
+      Log.e(TAG,
+            "NullPointerException trying to rename " + srcPath + " to " +
+                dstPath,
+            e);
+    }
+    return ok;
+  }
+}

ScreenOffUdfps/src/org/samsung/screenoffudfps/utils/Tags.java
@@ -1,0 +1,6 @@
+package org.samsung.screenoffudfps.utils;
+
+public class Tags {
+    public static final String SCREEN_OFF_UDFPS_KEY = "screen_off_udfps";
+    public static final String SCREEN_OFF_UDFPS_NODE = "/sys/class/sec/tsp/cmd";
+}